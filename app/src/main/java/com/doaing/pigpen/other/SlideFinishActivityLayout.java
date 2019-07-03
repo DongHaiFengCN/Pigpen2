81b07142cc8833bc350919ee131f7b3edd8feddb
your_name <31688286+DongHaiFengCN@users.noreply.github.com>
2019-07-03 10:16:15 +0800

    初步完成滑动关闭界面

--- app/src/main/java/com/doaing/pigpen/other/SlideFinishActivityLayout.java
+++ app/src/main/java/com/doaing/pigpen/other/SlideFinishActivityLayout.java
@@ -7,16 +7,18 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 public class SlideFinishActivityLayout extends LinearLayout {
 
-    private float lastPointX;
-    private ViewGroup mParentView;
+
     private int windowSize;
     private int maxSlideSize;
-    private int tranX;
+    private int lastX;
+
+    private int totalDistance;
 
     public SlideFinishActivityLayout(Context context) {
         super(context);
@@ -29,38 +31,46 @@ public class SlideFinishActivityLayout extends LinearLayout {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        if (changed) {
-            mParentView = (ViewGroup) this.getParent();
-            windowSize = this.getWidth();
-            maxSlideSize = windowSize / 3;
-        }
+        windowSize = this.getWidth();
+        maxSlideSize = windowSize / 3;
     }
 
     @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            lastPointX = ev.getX();
-            return true;
-        } else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
-            float changeX = ev.getX() - lastPointX;
-            if (changeX >= 0) {
-                tranX += changeX;
-                mParentView.scrollBy((int) -changeX, 0);
-            }
-            return true;
-        } else if (ev.getAction() == MotionEvent.ACTION_UP) {
+    public boolean onTouchEvent(MotionEvent event) {
 
-            if (tranX >= maxSlideSize) {
-                moveEnd(ev.getX());
-            } else {
-                moveStart(tranX);
-            }
-            return true;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) event.getRawX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int offX = (int) (event.getRawX() - lastX);
+                if (offX >= 0) {
+                    ((View) getParent()).scrollTo(-offX, 0);
+                } else {
+                    ((View) getParent()).scrollTo(0, 0);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+
+                int move = (int) (event.getRawX() - lastX);
+                if (move <= maxSlideSize) {
+
+                    ((View) getParent()).scrollTo(0, 0);
+
+                } else {
+                    ((View) getParent()).scrollTo(-windowSize, 0);
+                    Activity activity = (Activity) getContext();
+                    activity.finish();
+                }
+
+
+                break;
 
         }
-        return super.dispatchTouchEvent(ev);
-    }
 
+        return true;
+    }
+/*
     public void moveEnd(float tranX) {
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(this, "translationX", tranX, windowSize);
         objectAnimator.addListener(new Animator.AnimatorListener() {
@@ -86,32 +96,6 @@ public class SlideFinishActivityLayout extends LinearLayout {
 
             }
         });
-        objectAnimator.setDuration(200).start();//1 操纵的对象 2 所需要操纵的对象属性 3 动画变化的范围
-    }
-
-    public void moveStart(float tranX) {
-        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(this, "translationX", 0, -tranX);
-        objectAnimator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-        objectAnimator.setDuration(200).start();//1 操纵的对象 2 所需要操纵的对象属性 3 动画变化的范围
-    }
+        objectAnimator.setDuration(500).start();//1 操纵的对象 2 所需要操纵的对象属性 3 动画变化的范围
+    }*/
 }
